99c02067a190691716f238fad0afbaa471eb52bc
Morgan Pietruszka <mlp6bu@virginia.edu>
2014-04-13 23:17:57 +0000

    updated site info

--- handlers.js
+++ handlers.js
@@ -2,10 +2,18 @@ exports.index = function(req, res) {
 	res.send("welcome to CS1501");
 }
 
-exports.watch=function(req, res) {
+exports.watchf=function(req, res) {
 	var video_id = req.query.v;
 	res.render('watch', {id: video_id}, function(err, html){
 		res.send(html);
 
-	});
+	})
 }
+
+exports.bootstrap101 = function(req, res) {
+	res.render('bootstrap', function(err, html){
+		res.send(html);
+	})
+}
+
+exports.watch = watchf;
